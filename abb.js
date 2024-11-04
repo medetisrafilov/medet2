d9123dd4ecd388ae8472df71fd47397696ac1816
medetisrafilov <israfilovmedet52@gmail.com>
2024-11-04 00:14:42 -1200

    homework2

---
+++ abb.js
@@ -0,0 +1,13 @@
+let user =("Zehmet Olmasa Reqem DaxilEdin(Yanlizca 4 reqemden Ibaret Olsun)")
+
+
+if (user <=999){
+    userNum = Number(user[0])
+     userNum2 = Number(user[1])
+     userNum3 = Number(user[2])
+     userNum4 = Number(user[3])
+     numSum = userNum + userNum2 + userNum3 +userNum4
+     console.log(numSum);
+} else{
+    console.log("Reqemlerin Sayi 4-den Artiqdir (Yeinden cehd edin)")
+}
